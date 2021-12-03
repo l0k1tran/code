c202968f27125648cbc0a4da6ec006025f05f8ad
JosephHuy <huy.nguyenthanh>
2021-12-03 17:31:08 +0700

    first commit

---
+++ mix/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head();?>
+
+</head>
+
+<body <?php body_class(); ?>>
+    <div class="wrapper">
+        <div class="wrapper-header relative z-20">
+            <header class="relative z-10">
+                <div class=" absolute left-0 lg:left-[201px] top-[1px] ">
+                    <img src="/altamedia/wp-content/uploads/2021/11/Little-Little-Logo-ngang-1.png" alt="logo">
+                </div>
+
+                <div>
+                    <!-- <?php tittle_menu('primary-menu'); ?> -->
+                    <ul
+                        class="absolute z-20 top-7 w-full text-right sm:block lg:flex md:block justify-center item-center right-0 ">
+                        <li><a href="#">Trang chủ</a></li>
+                        <li><a href="#">Sự kiện</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                        <li><a href="#">0123456789</a></li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+
+        <div class=" object-cover absolute  left-1 top-20 lg:left-9 z-0">
+            <img src="/altamedia/wp-content/uploads/2021/11/bg.png" alt="">
+        </div>
+        <div class="absolute z-10 left-7 top-32 w-20 h-20 lg:left-[200px] lg:top-[153.59px] lg:w-48 lg:h-36">
+            <img src="/altamedia/wp-content/uploads/2021/11/image-2-1.png" alt=""
+                class="lg:max-h-36 lg:max-w-md max-w-min max-h-32">
+        </div>
+
+        <h1
+            class="absolute text-5xl lg:text-7xl font-black normal text-[#FFFFFF]  left-56 top-36 lg:left-[397px] lg:top-[137px] max-w-sm max-h-28 not-italic">
+            ĐẦM
+            SEN
+        </h1>
+        <h1
+            class="absolute  text-5xl lg:text-7xl font-black normal text-[#FFFFFF] left-56 top-52 lg:left-[402px] lg:top-[213px] w-52 h-28 not-italic">
+            PARK
+        </h1>
+        <div class="absolute w-20 h-20  top-72 lg:left-[18px] lg:top-[321px] lg:w-[147.37px] lg:h-[210px]">
+            <img src="/altamedia/wp-content/uploads/2021/11/18451-Converted-06-1.png" alt="">
+        </div>
+        <div class="absolute w-32 h-32 top-96 lg:top-[528px] lg:w-full lg:h-full z-10">
+            <img src="/altamedia/wp-content/uploads/2021/12/Lisa_Arnold_Lay_Do_F2-3-1.png" alt="">
+        </div>
+        <div class="absolute w-20 h-20 top-40 left-[29rem] xl:left-[807px] 2xl:top-[179.02px] 2xl:w-full 2xl:h-full">
+            <img src="/altamedia/wp-content/uploads/2021/11/18451-Converted-03-1.png" alt="">
+        </div>
+        <div class="absolute xl:left-[1029px] xl:top-[92.93px]">
+            <img src="/altamedia/wp-content/uploads/2021/11/18451-Converted-02-1.png" alt="">
+        </div>
+        <div class="absolute xl:left-[1052px] xl:top-[23px]">
+            <img src="/altamedia/wp-content/uploads/2021/11/render-fix-hair-1.png" alt="">
+        </div>
+        <div class="absolute xl:left-[630px] xl:top-[942.65px] ">
+            <img src="/altamedia/wp-content/uploads/2021/11/18451-Converted-04-1.png" alt="">
+        </div>
+        <div class="absolute z-10 xl:left-[1693px] xl:top-[878.04px]">
+            <img src="/altamedia/wp-content/uploads/2021/11/18451-Converted-03-1.png" alt="">
+        </div>
+        <div>
+            <img src="http://localhost/altamedia/wp-content/uploads/2021/11/Group.png" alt=""
+                class="absolute 2xl:w-[950px] 2xl:h-[600px] 2xl:left-[201px] 2xl: top-[356px] 2xl:bottom-[115px]">
+            <img src="http://localhost/altamedia/wp-content/uploads/2021/11/Vector.png" alt="">
+
+        </div>
+
+    </div>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
